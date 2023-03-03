3adfa59be776e031ef679aed98662fd836863474
Nyyberg <92079143+Nyyberg@users.noreply.github.com>
2023-03-03 18:19:17 +0100

    added input field and field that shows stack

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -24,6 +28,58 @@ class _CalculatorState extends State<Calculator> {
       appBar: AppBar(
         title: Center(child: Text("Calculator")),
       ) ,
+    body: Column(
+      children: [
+        Container(
+          height: 40,
+          color: Colors.white,
+        ),
+        Expanded(child:
+          Container(
+          width: double.infinity,
+          child: Scaffold(
+            backgroundColor: Colors.white,
+            body: Column(
+              children: <Widget>[
+                Expanded(child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(
+                        height: 80,
+                        child: Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black)),
+                          alignment: Alignment.center,
+                          child: Text("placeholderInput", style: TextStyle(fontSize: 25),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 80,
+                        width: 400,
+                        child: Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black)),
+                          alignment: Alignment.center,
+                          child: Text("placeholderStack", style: TextStyle(fontSize: 25),
+                          ),
+                        ),
+                      ),
+                      
+                    ],
+                  ),
+                ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        ),
+      ],
+    ),
 
     );
   }
